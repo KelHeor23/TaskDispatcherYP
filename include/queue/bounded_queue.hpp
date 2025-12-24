44f3b3531095112df1f6fea1b8fa8032ad76dba0
Alex <kelheor23@gmail.com>
2025-12-24 11:31:36 +0300

    BoundedQueue

--- include/queue/bounded_queue.hpp
+++ include/queue/bounded_queue.hpp
@@ -1,10 +1,20 @@
 #pragma once
 #include "queue/queue.hpp"
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <queue>
 
 namespace dispatcher::queue {
 
 class BoundedQueue : public IQueue {
-    // здесь ваш код
+private:
+    std::unique_ptr<QueueOptions> option_;
+    std::mutex mtx_;
+    std::condition_variable cv_not_full_;
+    std::condition_variable cv_not_empty_;
+    std::queue<std::function<void()>> queue_;
+
 public:
     explicit BoundedQueue(int capacity);
 
--- src/queue/bounded_queue.cpp
+++ src/queue/bounded_queue.cpp
@@ -1,7 +1,37 @@
 #include "queue/bounded_queue.hpp"
+#include <memory>
+#include <mutex>
+#include <optional>
 
 namespace dispatcher::queue {
 
-// здесь ваш код
+BoundedQueue::BoundedQueue(int capacity) {
+    if (capacity <= 0)
+        throw std::invalid_argument("Capacity must be positive");
+    option_ = std::make_unique<QueueOptions>(true, capacity);
+}
 
-} // namespace dispatcher::queue
+void BoundedQueue::push(std::function<void()> task) {
+    std::unique_lock lock(mtx_);
+
+    cv_not_full_.wait(lock, [this] { return queue_.size() < option_->capacity; });
+
+    queue_.push(std::move(task));
+    cv_not_empty_.notify_one();
+}
+
+std::optional<std::function<void()>> BoundedQueue::try_pop() {
+    std::unique_lock lock(mtx_);
+
+    cv_not_empty_.wait(lock, [this] { return !queue_.empty(); });
+
+    auto res = std::move(queue_.front());
+    queue_.pop();
+
+    cv_not_full_.notify_one();
+    return res;
+}
+
+BoundedQueue::~BoundedQueue() {}
+
+}  // namespace dispatcher::queue
