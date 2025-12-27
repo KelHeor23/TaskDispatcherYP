35001815f09ab6e84f56d90f6aba18a5bbdd3d7a
Alex <kelheor23@gmail.com>
2025-12-27 10:36:16 +0300

    TaskDispatcher

--- src/task_dispatcher.cpp
+++ src/task_dispatcher.cpp
@@ -1,7 +1,17 @@
 #include "task_dispatcher.hpp"
+#include <memory>
 
 namespace dispatcher {
 
-// здесь ваш код
+TaskDispatcher::TaskDispatcher(size_t thread_count,
+                               const std::unordered_map<TaskPriority, queue::QueueOptions> &params) {
+    priority_queue_ = std::make_shared<queue::PriorityQueue>(params);
+    thread_pool_ = std::make_unique<thread_pool::ThreadPool>(priority_queue_, thread_count);
+}
 
-} // namespace dispatcher
+void TaskDispatcher::schedule(TaskPriority priority, std::function<void()> task) {
+    priority_queue_->push(priority, task);
+}
+
+TaskDispatcher::~TaskDispatcher() { priority_queue_->shutdown(); }
+}  // namespace dispatcher
